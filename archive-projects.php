f59d15caa2f1baaae2bb78dfc343038d441ed704
Tashan Duncan <tashanduncan1994@gmail.com>
2022-03-21 20:05:33 +0000

    project archive added

---
+++ archive-projects.php
@@ -0,0 +1,22 @@
+<?php
+get_header()
+?>
+<section class="cta-section theme-bg-light py-5">
+    <div class="container text-center single-col-max-width">
+        <h2 class="heading">Portfolio</h2>
+        <div class="intro">
+            <p>Welcome to my online portfolio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. I'm taking on freelance work at the moment. Want some help building your software?</p>
+
+        </div>
+        <a class="btn btn-primary" href="contact.html" target="_blank"><i class="fas fa-paper-plane mr-2"></i>Contact Me</a>
+
+
+    </div>
+    <!--//container-->
+</section>
+<?php get_template_part('template-parts/content', 'projectarchive') ?>
+
+
+<?php
+get_footer()
+?>
---
+++ template-parts/content-projectarchive.php
@@ -0,0 +1,51 @@
+<section class="projects-list px-3 py-5 p-md-5">
+    <div class="container">
+        <div class="text-center">
+            <ul id="filters" class="filters mb-5 mx-auto pl-0">
+                <li class="type active mb-3 mb-lg-0" data-filter="*">All</li>
+                <li class="type  mb-3 mb-lg-0" data-filter=".webapp">We App</li>
+                <li class="type  mb-3 mb-lg-0" data-filter=".mobileapp">Mobile App</li>
+                <li class="type  mb-3 mb-lg-0" data-filter=".frontend">Frontend</li>
+                <li class="type  mb-3 mb-lg-0" data-filter=".backend">Backend</li>
+            </ul>
+            <!--//filters-->
+        </div>
+        <div class="project-cards row isotope">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php
+                    $intro = get_field('project_intro');
+                    $client = get_field('client_name');
+                    ?>
+                    <div class="isotope-item col-md-6 mb-5 mobileapp frontend">
+                        <div class="card project-card">
+                            <div class="row no-gutters">
+                                <div class="col-lg-4 card-img-holder">
+                                    <?php if (has_post_thumbnail()) : ?>
+                                        <img src="<?php the_post_thumbnail_url() ?>" class="card-img" alt="image">
+                                    <?php endif; ?>
+                                </div>
+                                <div class="col-lg-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><a href="<?php the_permalink(); ?>" class="theme-link"><?php the_title(); ?></a></h5>
+                                        <p class="card-text"><?php echo $intro; ?></p>
+                                        <p class="card-text"><small class="text-muted">Client: <?php echo $client; ?></small></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="link-mask">
+                                <a class="link-mask-link" href="project.html"></a>
+                                <div class="link-mask-text">
+                                    <a class="btn btn-secondary" href="<?php the_permalink(); ?>">
+                                        <i class="fas fa-eye mr-2"></i>View Case Study
+                                    </a>
+                                </div>
+                            </div>
+                            <!--//link-mask-->
+                        </div>
+                        <!--//card-->
+                    </div>
+                    <!--//col-->
+
+            <?php endwhile;
+            else : endif; ?>
+        </div>
